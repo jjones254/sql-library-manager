83c7e5f3436a18202bd84f1999b2b3060fb35e5f
jjones254 <justinjones254@gmail.com>
2022-01-28 10:02:27 -0600

    test connection and sync the database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var db = require('./models');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// test connection and sync the database
+(async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    await db.sequelize.sync();
+    console.log('The database has been synced successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}) ();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
