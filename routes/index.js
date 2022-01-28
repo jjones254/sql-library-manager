31d2e003df202afaa54c5e30ec0917341bb7478e
jjones254 <justinjones254@gmail.com>
2022-01-28 14:21:18 -0600

    test Book model and comm with database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const Book = require('../models').Book;
+
+/* async handler for each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      next(error);
+    }
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+  // res.render('index', { title: 'Express' });
+  const books = await Book.findAll();
+  res.json(books);
+}));
 
 module.exports = router;
