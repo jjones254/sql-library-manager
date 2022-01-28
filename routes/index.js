63cbed44229706f32b2d839dfe16df122d0ee02f
jjones254 <justinjones254@gmail.com>
2022-01-28 14:54:14 -0600

    var to const

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const Book = require('../models').Book;
 
 /* async handler for each route. */
